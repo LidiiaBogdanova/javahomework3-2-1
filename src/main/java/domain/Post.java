4ae6fde5c14c018894ffe2f650ab063e9ec14143
LidiiaBogdanova <novikova71095@gmail.com>
2021-04-16 01:34:45 +0300

    v1

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,16 @@
+package domain;
+
+import java.util.Date;
+
+public class Post {
+    private int id;
+    private String owner;
+    private Date date;
+    private String textURL;
+    private String text;
+    private CommentsInfo commentsInfo;
+    private LikesInfo likesInfo;
+    private RepostsInfo repostsInfo;
+    private ViewsInfo viewsInfo;
+
+}
